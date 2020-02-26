f60ef4c347eccfa60139e96ab2d03461e087088c
AlexeyKobets <kobets@gmail.com>
2020-02-25 16:12:16 -0800

    added development enviroment preparation script

---
+++ scripts/prep-devel.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+VGOPATH=$(echo $GOPATH | sed -e 's/:.*$//')
+
+if [ "x${VGOPATH}" == "x" ]; then
+	echo "Set GOPATH to somewhere" >&2
+	exit 0
+fi
+
+set -x
+set -e
+
+go get github.com/dgrijalva/jwt-go
+go get gopkg.in/yaml.v2
+go get go.uber.org/zap
+go get github.com/gorilla/mux
+go get gopkg.in/mgo.v2
+
