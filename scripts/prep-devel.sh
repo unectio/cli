f3c813a89a78a6c5ddab8d31c992dc86152f3cd3
AlexeyKobets <kobets@gmail.com>
2020-04-20 17:25:06 -0700

    removed debug info

--- scripts/prep-devel.sh
+++ scripts/prep-devel.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 VGOPATH=$(echo $GOPATH | sed -e 's/:.*$//')
 
-if [ "x${1}" != "x" ]; then
-	echo "Set GOPATH to ${1}"
-	VGOPATH=${1}
-    GOPATH=${1}
-fi
-
 if [ "x${VGOPATH}" == "x" ]; then
 	echo "Set GOPATH to somewhere" >&2
 	exit 0
@@ -16,23 +10,12 @@ set -x
 set -e
 
 go get github.com/spf13/cobra
-echo $?
-ls -l ${GOPATH}"/src/github.com/spf13/cobra"
 go get github.com/dgrijalva/jwt-go
-echo $?
-ls -l ${GOPATH}"/src/github.com/dgrijalva/jwt-go"
 go get gopkg.in/yaml.v2
-echo $?
 go get go.uber.org/zap
-echo $?
 go get github.com/gorilla/mux
-echo $?
 go get gopkg.in/mgo.v2
-echo $?
 
 go get github.com/unectio/db
-echo $?
 go get github.com/unectio/api
-echo $?
 go get github.com/unectio/util
-echo $?
